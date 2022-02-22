4d37fe815cf729fc2772f232a95ed8cef9406fcc
Bruno Colin <brunocolinxbox@hotmail.com>
2022-02-22 18:53:32 -0300

    admin sign in and sing out

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    root to: "home#index"
+  end
+
+end
